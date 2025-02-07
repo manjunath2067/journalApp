489f8ea10ebae3e259544be729e56af81b42494c
Tirakannavar Manjunath Basagouda (MS/EDV4-ETAS) <ManjunathBasagouda.Tirakannavar@in.bosch.com>
2025-02-07 12:38:48 +0530

    Moved the TransactionManagement to config package.

--- src/main/java/com/learning/journalApp/JournalApplication.java
+++ src/main/java/com/learning/journalApp/JournalApplication.java
@@ -2,30 +2,12 @@ package com.learning.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class JournalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JournalApplication.class, args);
-	}
-
-	/**
-	 * This method will create a bean of type PlatformTransactionManager
-	 * This PlatformTransactionManager will be used to manage transactions in the application
-	 * wherever Transactional annotation is used
-	 * @param dbFactory
-	 * @return
-	 */
-	@Bean
-	public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
-		return new MongoTransactionManager(dbFactory);
 	}
 
 }

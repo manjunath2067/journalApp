a25c37736ef54b77dde2df1a0083588ad7f33b7f
Tirakannavar Manjunath Basagouda (MS/EDV4-ETAS) <ManjunathBasagouda.Tirakannavar@in.bosch.com>
2025-02-07 12:31:19 +0530

    This commit includes CRUD operations for journal entry with mongoDB setup.

---
+++ src/main/java/com/learning/journalApp/service/UserService.java
@@ -0,0 +1,39 @@
+package com.learning.journalApp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learning.journalApp.entity.User;
+import com.learning.journalApp.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveEntry(User user) {
+        userRepository.save(user);
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(ObjectId id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteUserById(ObjectId id) {
+        userRepository.deleteById(id);
+    }
+
+    public User findByUserName(String userName) {
+        return userRepository.findByUserName(userName);
+    }
+
+}

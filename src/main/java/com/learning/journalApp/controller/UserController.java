75a7d3ac6243531a3b2b8e2afcd3f7f9882fda9e
Manjunath Tirakannavar <manjunathbt2067@gmail.com>
2025-02-18 15:53:42 +0530

    feat: Update user endpoints with Spring Security
    - Added authentication to updateUser and deleteUserById endpoints
    - Ensured user information is updated and deleted based on authenticated user
    - User creation can be done without any authentication

--- src/main/java/com/learning/journalApp/controller/UserController.java
+++ src/main/java/com/learning/journalApp/controller/UserController.java
@@ -1,18 +1,18 @@
 package com.learning.journalApp.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.journalApp.entity.User;
+import com.learning.journalApp.repository.UserRepository;
 import com.learning.journalApp.service.UserService;
 
 @RestController
@@ -21,28 +21,34 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping
-    public List<User> getAll() {
-        return userService.getAll();
-    }
-
-    @PostMapping
-    public void createUser(@RequestBody User user) {
-        userService.saveEntry(user);
-    }
-
-    @PutMapping("/{userName}")
+    /**
+     * Updates the user information.
+     * Remember while updating info, put userName and password in auth field then change the body.
+     *
+     * @param user the user details to update
+     * @return a ResponseEntity with HTTP status NO_CONTENT
+     */
+    @PutMapping
     public ResponseEntity<?> updateUser(@RequestBody User user) {
-        User userInDb = userService.findByUserName(user.getUserName());
-        if (userInDb != null) {
-            userInDb.setUserName(user.getUserName());
-            userInDb.setPassword(user.getPassword());
-            userService.saveEntry(userInDb);
-        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        User userInDb = userService.findByUserName(userName);
+        userInDb.setUserName(user.getUserName());
+        userInDb.setPassword(user.getPassword());
+        userService.saveEntry(userInDb);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @DeleteMapping
+    public ResponseEntity<?> deleteUserById() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        userRepository.deleteByUserName(userName);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
 

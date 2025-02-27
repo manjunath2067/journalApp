351bde89f1eb05277443f8187cbb27d94045dbeb
Manjunath Tirakannavar <manjunathbt2067@gmail.com>
2025-02-27 12:39:47 +0530

    feat: Adapted logger

--- src/main/java/com/learning/journalApp/controller/PublicController.java
+++ src/main/java/com/learning/journalApp/controller/PublicController.java
@@ -1,6 +1,9 @@
 package com.learning.journalApp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.journalApp.entity.User;
 import com.learning.journalApp.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 @RequestMapping("/public")
 public class PublicController {
 
@@ -23,8 +28,10 @@ public class PublicController {
     }
 
     @PostMapping("/create-user")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.saveNewUser(user);
+        log.info("User created successfully: {}", user.getUserName());
+        return ResponseEntity.ok("User created successfully");
+    }
     }
 
-}
